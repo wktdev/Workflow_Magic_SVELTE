f109e687f36deb47844e82419f92eb6d56d1da26
wktdev <wktdev@pop-os.localdomain>
2021-12-16 20:28:36 -0600

    building event list

--- src/storageAPI/indexedDB.js
+++ src/storageAPI/indexedDB.js
@@ -1,102 +1,117 @@
-import Dexie from 'dexie';
+import Dexie from 'dexie'
 
-var indexedDB = new Dexie("WorkflowMagicUserDB");
+var indexedDB = new Dexie('WorkflowMagicUserDB')
 indexedDB.version(1).stores({
-    workflows: "++id,client_id,content,title",
-    clients: "++id,name",
-    // contacts:"++id,client_id,content,title",
-    contacts: "++id,client_id,full_name,first_name,last_name,email,phone_number,additional_information",
+  workflows: '++id,client_id,content,title',
+  clients: '++id,name',
+  // contacts:"++id,client_id,content,title",
+  contacts:
+    '++id,client_id,full_name,first_name,last_name,email,phone_number,additional_information',
 
-    calendar_events: "++id,start,end,title,description,client_name,client_id,group_id",
-    calendar_event_group_id: "++id, title, client_name, client_id",
-    user: "user_id"
-});
-
-
-
+  calendar_events:
+    '++id,start,end,title,description,client_name,client_id,group_id',
+  calendar_event_group_id: '++id, title, client_name, client_id',
+  user: 'user_id',
+})
 
 // export async function getAllCalendarEvents() {
 //     let result = await indexedDB.calendar_events.toArray();
 //     return result // returns an entire array (not individual objects)
 // }
 
-
-export async function createCalendarEvent(name) {
-    let result = await indexedDB.calendar_events.add({ title: name });
-    return result
+export async function createCalendarEvent(
+  startDate,
+  endDate,
+  title,
+  description,
+  clientName,
+  clientId,
+  groupId,
+) {
+  let result = await indexedDB.calendar_events.add({
+    start: startDate,
+    end: endDate,
+    title: title,
+    description: description,
+    client_name: clientName,
+    client_id: clientId,
+    group_id: groupId,
+  })
+  return result
 }
+
+
+
 
 export async function getClientCalendarEvents(clientID) {
+  let allCalendarEvents = await indexedDB.calendar_events.toArray()
+  let correctCalendarEvents = await allCalendarEvents.filter((val) => {
+    return val.client_id === clientID
+  })
 
-    let allCalendarEvents = await indexedDB.calendar_events.toArray();
-    let correctCalendarEvents= await allCalendarEvents.filter((val) => {
-        return val.client_id === clientID
-    });
-
-    return correctCalendarEvents
+  return correctCalendarEvents
 }
 
 
+export async function deleteCalendarEvent(id) {
+  let result = await indexedDB.calendar_events.where('id').equals(id).delete()
+  return result
+}
 
 export async function createClient(name) {
-    let result = await indexedDB.clients.add({ name: name });
-    return result
+  let result = await indexedDB.clients.add({ name: name })
+  return result
 }
 
 export async function deleteClient(id) {
-    let result = await indexedDB.clients.where("id").equals(id).delete();
-    return result
+  let result = await indexedDB.clients.where('id').equals(id).delete()
+  return result
 }
 
-
-
-
-
-
 export async function getAllClients() {
-    let result = await indexedDB.clients.toArray();
-    return result // returns an entire array (not individual objects)
+  let result = await indexedDB.clients.toArray()
+  return result // returns an entire array (not individual objects)
 }
 
 export async function getClientById(id) {
-    let result = await indexedDB.clients.get(id);
-    console.log(result)
-    return result
+  let result = await indexedDB.clients.get(id)
+  console.log(result)
+  return result
 }
 
-
 export async function createWorkflow(clientID, title, content) {
-    let result = await indexedDB.workflows.add({ title: title, content: content, client_id: clientID });
-    return result
+  let result = await indexedDB.workflows.add({
+    title: title,
+    content: content,
+    client_id: clientID,
+  })
+  return result
 }
 
 export async function getClientWorkflows(clientID) {
-    let allWorkflows = await indexedDB.workflows.toArray();
-    let correctWorkflows = await allWorkflows.filter((val) => {
-        return val.client_id === clientID
-    });
+  let allWorkflows = await indexedDB.workflows.toArray()
+  let correctWorkflows = await allWorkflows.filter((val) => {
+    return val.client_id === clientID
+  })
 
-    return correctWorkflows
+  return correctWorkflows
 }
-
-
 
 export async function updateWorkflow(id, title, content) {
-    let result = await indexedDB.workflows.update(id, { title: title, content: content })
-    return result
+  let result = await indexedDB.workflows.update(id, {
+    title: title,
+    content: content,
+  })
+  return result
 }
-
 
 export async function getWorkflowById(id) {
-    let result = await indexedDB.workflows.get(id);
-    console.log(result)
-    return result
+  let result = await indexedDB.workflows.get(id)
+  console.log(result)
+  return result
 }
-
-
 
 export async function deleteWorkflow(id) {
-    let result = await indexedDB.workflows.where("id").equals(id).delete();
-    return result
+  let result = await indexedDB.workflows.where('id').equals(id).delete()
+  return result
 }
-
--- src/views/CalendarEvents.svelte
+++ src/views/CalendarEvents.svelte
@@ -1,9 +1,11 @@
 <script>
   import DatePicker from "@beyonk/svelte-datepicker/src/components/DatePicker.svelte";
-
+  import Dexie from "dexie";
   import SearchAndCreateField from "../components/SearchAndCreateField.svelte";
   import { createWorkflow } from "../storageAPI/indexedDB";
   import {
+    deleteCalendarEvent,
+    createCalendarEvent,
     getClientCalendarEvents,
     getClientById,
     deleteWorkflow,
@@ -12,63 +14,89 @@
   export let params = {};
 
   let calendarEvents = [];
-  let clientID;
+  let clientID = parseInt(params.clientId);
   let clientName;
-  let allClientCalendarEvents = [];
+  $: allClientCalendarEvents = [];
 
   onMount(async () => {
-    clientID = params.clientId;
-    //___________________________________________________BEGIN replace with calendar events
-    // getClientWorkflows(clientID).then((result) => {
-    //   workflows = [...result];
-    // });
-    //____________________________________________________END
+
+console.log(params)
+
+console.log(clientID)
+    getClientById(clientID).then((obj)=>{
+      console.log(obj)
+       clientName = obj.name
+    })
+
+
+    console.log(params)
+    clientID = parseInt(params.clientId);
+
 
     await getClientCalendarEvents(parseInt(params.clientId)).then(
       (calendarEvents) => {
         allClientCalendarEvents = calendarEvents;
+
+        console.log(calendarEvents)
       }
     );
 
-    await getClientById(parseInt(params.clientId)).then((client) => {
-      clientName = client.name;
-    });
+
+
   });
 
-  async function submitToDatabase(item) {
+  async function submitToDatabase(title) {
     //___________________________________________________BEGIN replace with calendar events
     // let workflowID = await createWorkflow(clientID, item); // create new client
     //________________________________________________________END
 
-    await getClientCalendarWorkflows(clientID).then((result) => {
-      workflows = [...result.reverse()]; // store new state to page
+    await createCalendarEvent("2021-12-25T06:00:00.000Z","2021-12-25T06:00:00.000Z",title,"Description test","test client name",clientID,"group-data");
+
+    await getClientCalendarEvents(parseInt(params.clientId)).then((events)=>{
+        let eventsOrdered = events.reverse();
+          allClientCalendarEvents = [...eventsOrdered ]
     });
-    window.location.href =
-      "#/client/" + clientID + "/dashboard/calendar-events/" + calendarID + "/edit";
+
+
+  // 2021-12-25T06:00:00.000Z
+
+
+    // window.location.href =
+    // "#/client/" + clientID + "/dashboard/calendar-events/" + calendarID + "/edit";
   }
 
   function goToRoute(item) {
-    let calendarEventID = item.id;
-    if (item.content === undefined) {
-      window.location.href =
-        "#/client/" +
-        clientID +
-        "/calendar-events/" +
-        calendarEventID +
-        "/edit";
-    } else {
-      window.location.href =
-        "#/client/" + clientID + "/calendar-events/" + calendarEventID;
-    }
+    console.log(item);
+
+    window.location.href = "#/client/" + item.id + "/dashboard/calendar-events";
   }
 
+  // function goToRoute(item) {
+  //   window.location.href =
+  //       "#/client/" +
+  //       clientID +
+  //       "/dashboard/calendar-events/"
+  //   // let calendarEventID = item.id;
+  //   // if (item.title === undefined) {
+  //   //   window.location.href =
+  //   //     "#/client/" +
+  //   //     clientID +
+  //   //     "/dashboard/calendar-events/" +
+  //   //     calendarEventID +
+  //   //     "/edit";
+  //   // } else {
+  //   //   window.location.href =
+  //   //     "#/client/" + clientID + "/dashboard/calendar-events/" + calendarEventID;
+  //   // }
+  // }
+
   async function onDelete(id) {
-    // let workflowID = workflows[id].id;
-    // await deleteWorkflow(workflowID);
-    // await getClientWorkflows(clientID).then((result) => {
-    //   let list = result.reverse();
-    //   workflows = [...list];
-    // });
+    let calendarEventID = allClientCalendarEvents[id].id;
+    await deleteCalendarEvent(calendarEventID);
+    await getClientCalendarEvents(clientID).then((result) => {
+      let list = result.reverse();
+     allClientCalendarEvents = [...list];
+    });
   }
 
   let selected;
@@ -82,7 +110,7 @@
     <div class="row">
       <div class="col-0" />
       <div class="col-12">
-        <h1 class="client-name">Workflows</h1>
+        <h1 class="client-name">Events & Meetings</h1>
         <h2 class="logo-title">{clientName}</h2>
       </div>
       <div class="col-0" />
